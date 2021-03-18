8d42c70fbc452bc263f2b7f2deec8705ccc375d9
ahmedAbdlsalam <ahmedmoabdelsalam98@gmail.com>
2021-03-18 19:51:57 +0200

    2commit

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'shop'
 
@@ -10,3 +12,7 @@ urlpatterns = [
     path('<int:id>/<slug:slug>/', views.product_detail,
          name='product_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
