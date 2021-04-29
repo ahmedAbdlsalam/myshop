c978f48f5e76d5f2f6784237e778842d0afae695
ahmedAbdlsalam <ahmedmoabdelsalam98@gmail.com>
2021-04-29 16:10:01 +0200

    adding cart functionality

--- cart/views.py
+++ cart/views.py
@@ -5,6 +5,7 @@ from .cart import Cart
 from .forms import CartAddProductForm
 
 
+
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
@@ -28,4 +29,8 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={
+            'quantity': item['quantity'],
+            'override': True})
     return render(request, 'cart/detail.html', {'cart': cart})
